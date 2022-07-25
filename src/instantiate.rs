f4666f11e736d94ea885927f36814ee81b041f46
tsilva@figure.com <74169378+tsilva-figure@users.noreply.github.com>
2022-07-25 12:17:00 -0600

    better assertions

--- src/instantiate.rs
+++ src/instantiate.rs
@@ -70,6 +70,7 @@ mod tests {
     use crate::msg::QueryMsg;
     use crate::msg::RaiseState;
     use crate::query::query;
+    use cosmwasm_std::coin;
     use cosmwasm_std::testing::MOCK_CONTRACT_ADDR;
     use cosmwasm_std::testing::{mock_env, mock_info};
     use cosmwasm_std::{from_binary, Addr};
@@ -99,38 +100,88 @@ mod tests {
 
         // verify that 8 messages are sent to configure 2 new markers
         assert_eq!(8, res.messages.len());
-        assert!(matches!(
+
+        // verify create commitment marker
+        let commitment_denom = format!("{}.commitment", MOCK_CONTRACT_ADDR);
+        assert_eq!(
+            &MarkerMsgParams::CreateMarker {
+                coin: coin(0, commitment_denom.clone()),
+                marker_type: MarkerType::Coin
+            },
             marker_msg(msg_at_index(&res, 0)),
-            MarkerMsgParams::CreateMarker { .. }
-        ));
-        assert!(matches!(
+        );
+
+        // verify grant commitment marker permissions
+        assert_eq!(
+            &MarkerMsgParams::GrantMarkerAccess {
+                denom: commitment_denom.clone(),
+                address: Addr::unchecked(MOCK_CONTRACT_ADDR),
+                permissions: vec![
+                    MarkerAccess::Admin,
+                    MarkerAccess::Mint,
+                    MarkerAccess::Burn,
+                    MarkerAccess::Withdraw,
+                ],
+            },
             marker_msg(msg_at_index(&res, 1)),
-            MarkerMsgParams::GrantMarkerAccess { .. }
-        ));
-        assert!(matches!(
+        );
+
+        // verify finalize commitment marker
+        assert_eq!(
+            &MarkerMsgParams::FinalizeMarker {
+                denom: commitment_denom.clone()
+            },
             marker_msg(msg_at_index(&res, 2)),
-            MarkerMsgParams::FinalizeMarker { .. }
-        ));
-        assert!(matches!(
+        );
+
+        // verify activate commitment marker
+        assert_eq!(
+            &MarkerMsgParams::ActivateMarker {
+                denom: commitment_denom
+            },
             marker_msg(msg_at_index(&res, 3)),
-            MarkerMsgParams::ActivateMarker { .. }
-        ));
-        assert!(matches!(
+        );
+
+        // verify create investment marker
+        let investment_denom = format!("{}.investment", MOCK_CONTRACT_ADDR);
+        assert_eq!(
+            &MarkerMsgParams::CreateMarker {
+                coin: coin(0, investment_denom.clone()),
+                marker_type: MarkerType::Coin
+            },
             marker_msg(msg_at_index(&res, 4)),
-            MarkerMsgParams::CreateMarker { .. }
-        ));
-        assert!(matches!(
+        );
+
+        // verify grant investment marker permissions
+        assert_eq!(
+            &MarkerMsgParams::GrantMarkerAccess {
+                denom: investment_denom.clone(),
+                address: Addr::unchecked(MOCK_CONTRACT_ADDR),
+                permissions: vec![
+                    MarkerAccess::Admin,
+                    MarkerAccess::Mint,
+                    MarkerAccess::Burn,
+                    MarkerAccess::Withdraw,
+                ],
+            },
             marker_msg(msg_at_index(&res, 5)),
-            MarkerMsgParams::GrantMarkerAccess { .. }
-        ));
-        assert!(matches!(
+        );
+
+        // verify finalize investment marker
+        assert_eq!(
+            &MarkerMsgParams::FinalizeMarker {
+                denom: investment_denom.clone()
+            },
             marker_msg(msg_at_index(&res, 6)),
-            MarkerMsgParams::FinalizeMarker { .. }
-        ));
-        assert!(matches!(
+        );
+
+        // verify activate investment marker
+        assert_eq!(
+            &MarkerMsgParams::ActivateMarker {
+                denom: investment_denom
+            },
             marker_msg(msg_at_index(&res, 7)),
-            MarkerMsgParams::ActivateMarker { .. }
-        ));
+        );
 
         // verify that terms of raise are correct
         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetState {}).unwrap();
